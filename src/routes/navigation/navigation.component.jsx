7dd9586804ad52ff76cbc859c2b466538a8cef00
iMadfxq <imadfxq@gmail.com>
2022-04-10 21:06:01 -0500

    Added react router, started using it. Also using react fragments

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from "react-router-dom"; //Outlet is where children from routes will render
+import { Fragment } from "react";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import './navigation.styles.scss'
+
+const Navigation = () => {
+  return (
+    // it is very useful when we don't want to render a div, and it is necesary beacause react can only render one parent level
+    <Fragment>
+      <header>
+        <Link to={'/'}>
+          <CrownLogo />
+        </Link>
+        <div className="header--links">
+          <Link to={'/shop'}>
+            shop
+          </Link>
+        </div>
+      </header>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
