eb5036415268bbd1406b99ea11cda4c5958de73a
iMadfxq <imadfxq@gmail.com>
2022-04-12 15:31:29 -0500

    Added authentication and user storage on database via Firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAO1GoQovFYD9ThuE7zRp59-aNm3kRKYzc",
+  authDomain: "react-ecommerce-db-b2a99.firebaseapp.com",
+  projectId: "react-ecommerce-db-b2a99",
+  storageBucket: "react-ecommerce-db-b2a99.appspot.com",
+  messagingSenderId: "380780958735",
+  appId: "1:380780958735:web:c750a09bc0505d7155ff7a",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  //if user data document doesn't exist
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
